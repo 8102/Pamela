0c1766a5cf2f32f14c05505d3e4b45de16bf7ed6
Jean-Baptiste Grégoire <j.baptiste159@gmail.com>
2015-11-21 01:39:31 +0100

    Add configuration and sample of config file

--- pamela.py
+++ pamela.py
@@ -1,3 +1,29 @@
+from os import environ
+from os.path import isfile
+
+pathTest = './pamela.conf'
+pathProd = isfile('/home/' + environ['USER'] + '.config/pamela/pamela.conf')
+
+def loadConfig():
+	config = {
+		'container_name': "myContainer",
+		'container_path': "/home/" + environ['USER'],
+		'encryption_level': "medium"
+	}
+	if pathTest:
+		with open(pathTest, 'r') as f:
+			buf = f.readlines()
+			for line in buf:
+				line = str.strip(line, '\n')
+				tokens = str.split(line, '=')
+				if tokens[0] == 'container_name':
+					config['container_name'] = tokens[1]
+				elif tokens[0] == 'container_path':
+					config['container_path'] = tokens[1]
+				elif tokens[0] == 'encryption_level':
+					config['encryption_level'] = tokens[1]
+	return config
+
 def    pam_sm_authenticate(pamh, flags, argv):
     return pamh.PAM_SUCCESS
 
@@ -15,3 +41,6 @@ def    pam_sm_close_session(pamh, flags, argv):
 
 def    pam_sm_chauthtok(pamh, flags, argv):
     return pamh.PAM_SUCCESS
+
+config = loadConfig()
+print(config)
